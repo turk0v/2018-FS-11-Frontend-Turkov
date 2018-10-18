5821f2c04680947604386f1de839dc06584626c6
turk0v <turkoov@gmail.com>
2018-10-18 22:17:38 +0300

    added message send interface

--- src/lib/components/message-form/index.js
+++ src/lib/components/message-form/index.js
@@ -7,60 +7,63 @@ const template = `
 	<style>${shadowStyles.toString()}</style>
 	<form>
 		<div class="result"></div>
-		<form-input name="message_text" placeholder="Сообщение" slot="message-input">
+		<form-input class="form-input" name="message_text" placeholder="Отправить сообщение" slot="message-input">
 			<span slot="icon"></span>
 		</form-input>
 	</form>
 `;
 
 class MessageForm extends HTMLElement {
-	constructor () {
-		super();
-		const shadowRoot = this.attachShadow({mode: 'open'});
-		shadowRoot.innerHTML = template;
-		this._initElements();
-		this._addHandlers();
-	}
+    constructor() {
+        super();
+        const shadowRoot = this.attachShadow({ mode: 'open' });
+        shadowRoot.innerHTML = template;
+        this._initElements();
+        this._addHandlers();
+    }
 
-	static get observedAttributes() {
-		return [
-			"action",
-			"method"
-		]
-	}
+    static get observedAttributes() {
+        return [
+            'action',
+            'method',
+        ];
+    }
 
-	attributeChangedCallback(attrName, oldVal, newVal) {
-		this._elements.form[attrName] = newVal;
-	}
+    attributeChangedCallback(attrName, oldVal, newVal) {
+        this._elements.form[attrName] = newVal;
+    }
 
-	_initElements () {
-		var form = this.shadowRoot.querySelector('form');
-		var message = this.shadowRoot.querySelector('.result');
-		this._elements = {
-			form: form,
-			message: message
-		};
-	}
+    _initElements() {
+        const form = this.shadowRoot.querySelector('form');
+        const form_input = this.shadowRoot.querySelector('.form-input');
+        const message = this.shadowRoot.querySelector('.result');
+        this._elements = {
+            form,
+            form_input,
+            message,
+        };
+    }
 
-	_addHandlers () {
-		this._elements.form.addEventListener('submit', this._onSubmit.bind(this));
-		this._elements.form.addEventListener('keypress', this._onKeyPress.bind(this));
-		//this._elements.inputSlot.addEventListener('slotchange', this._onSlotChange.bind(this));
-	}
+    _addHandlers() {
+        this._elements.form.addEventListener('submit', this._onSubmit.bind(this));
+        this._elements.form.addEventListener('keypress', this._onKeyPress.bind(this));
+        // this._elements.inputSlot.addEventListener('slotchange', this._onSlotChange.bind(this));
+    }
 
-	_onSubmit (event) {
-		this._elements.message.innerText = Array.from(this._elements.form.elements).map(
-			el => el.value
-		).join(', ');
-		event.preventDefault();
-		return false;
-	}
+    _onSubmit(event) {
+        this._elements.message.innerText = Array.from(this._elements.form.elements).map(
+            el => el.value,
+        ).join(', ');
+        this._elements.form_input._elements.input.value = "";
+        event.preventDefault();
+        return false;
+    }
 
-	_onKeyPress (event) {
-		if (event.keyCode == 13) {
-			this._elements.form.dispatchEvent(new Event('submit'));
-		}
-	}
+    _onKeyPress(event) {
+        if (event.keyCode == 13) {
+            this._elements.form.dispatchEvent(new Event('submit'));
+        }
+    }
 }
 
 customElements.define('message-form', MessageForm);
